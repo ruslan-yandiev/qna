7be82cdf58964439498a20c7337367063ad7ee12
ruslan <sqwer1986@gmail.com>
2020-08-13 17:35:49 +0300

    creating a feature test: User can sign out

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,8 +1,11 @@
 module FeatureHelpers
   def sign_in(user)
+    # укажем хелпер маршрута new_user_session либо '/users/sign_in' созданный gem 'devise'
     visit new_user_session_path
+    # fill_in, click_on DSL гема capybara
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
+    # универсальный хелпер умеющий кликать и по кнопкам и по ссылкам
     click_on 'Log in'
   end
 end
