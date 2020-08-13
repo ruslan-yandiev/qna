00b56e1f894eae4d987088fb9fa2f77d947f1f7d
ruslan <sqwer1986@gmail.com>
2020-08-13 23:33:43 +0300

    creating a feature test: User can sign up

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 
 feature 'User can sign out'do
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -8,4 +8,18 @@ module FeatureHelpers
     # универсальный хелпер умеющий кликать и по кнопкам и по ссылкам
     click_on 'Log in'
   end
+
+  def sign_up(*params)
+    if params[0].class == String
+      user = User.new(email: params[0], password: params[1], password_confirmation: params[2])
+    else
+      user = params[0]
+    end
+
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+  end
 end
