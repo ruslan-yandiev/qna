6c76aef715b8ad2ab97cf23d2b5c8d3bdc13613d
ruslan <sqwer1986@gmail.com>
2020-08-24 07:28:08 +0300

    The author of the question can add files when editing his question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, except: %i[index show]
-  before_action :load_question, only: [:show, :edit, :update, :destroy]
+  before_action :load_question, only: :show
 
   expose :questions, -> { Question.all }
   expose :question
