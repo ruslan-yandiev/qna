611594de4d8025fd3bcdec1409dea36b84616f2b
ruslan <sqwer1986@gmail.com>
2020-08-29 17:42:55 +0300

    When adding links, check them against the URL format

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,4 +4,5 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates_format_of :url, with: URI.regexp(['http', 'https', 'ftp'])
 end
