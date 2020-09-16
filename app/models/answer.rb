9745f7e454ea014c01330ce8c72cf3a429d6913b
ruslan <sqwer1986@gmail.com>
2020-09-17 02:15:33 +0300

    create concern a linkable

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,13 +1,11 @@
 class Answer < ApplicationRecord
   include Votable
+  include Linkable
 
   belongs_to :question
   belongs_to :user
-  has_many :links, dependent: :destroy, as: :linkable
 
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   validates :body, presence: true
 
